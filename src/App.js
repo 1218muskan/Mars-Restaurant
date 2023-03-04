8aa4df85dfbc901b327484adca6db30833c780cd
Muskan Gupta <1218.muskangupta@gmail.com>
2023-03-04 15:19:31 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import './App.css';
+import Landing from './pages/landing';
+
+function App() {
+  return (
+    <div className="App">
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
