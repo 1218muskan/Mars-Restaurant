2628d381e0c4a1bb989cdb3c2ffac4dc47dd167b
Muskan Gupta <1218.muskangupta@gmail.com>
2023-03-05 05:57:36 +0530

    Home Page Menu

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import MarsOasis from '../Images/MarsOasis.gif'
+import Marquee from "react-fast-marquee";
 
 function Home() {
     return ( 
@@ -23,6 +24,88 @@ function Home() {
 
     <div className="gpt3__header-image">
       <img src={MarsOasis} />
+    </div>
+  </div>
+
+
+  {/* MENU */}
+
+  <div className="gpt3__blog section__padding" id="blog">
+    <div className="gpt3__blog-heading">
+      <h1 className="gradient__text">MENU</h1>
+    </div>
+    <div className="gpt3__blog-container">
+      {/* <div className="gpt3__blog-container_groupA">
+        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+      <div className="gpt3__blog-container_groupB">
+        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div> */}
+
+
+<div className='card-main'>
+          <FlipCard>
+            <FlipCard.Front className="cardFront card">
+              <h3>FEATURE NAME</h3>
+            </FlipCard.Front>
+            <FlipCard.Back className="cardBack card">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dicta, quasi soluta deleniti, quam sunt dolorum, impedit magnam tempora suscipit quod error fugit temporibus omnis hic ullam modi corrupti placeat?</p>
+            </FlipCard.Back>
+          </FlipCard>
+        </div>
+
+
+        <div className='card-main'>
+          <FlipCard>
+            <FlipCard.Front className="cardFront card">
+              <h3>FEATURE NAME</h3>
+            </FlipCard.Front>
+            <FlipCard.Back className="cardBack card">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dicta, quasi soluta deleniti, quam sunt dolorum, impedit magnam tempora suscipit quod error fugit temporibus omnis hic ullam modi corrupti placeat?</p>
+            </FlipCard.Back>
+          </FlipCard>
+        </div>
+
+
+        <div className='card-main'>
+          <FlipCard>
+            <FlipCard.Front className="cardFront card">
+              <h3>FEATURE NAME</h3>
+            </FlipCard.Front>
+            <FlipCard.Back className="cardBack card">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dicta, quasi soluta deleniti, quam sunt dolorum, impedit magnam tempora suscipit quod error fugit temporibus omnis hic ullam modi corrupti placeat?</p>
+            </FlipCard.Back>
+          </FlipCard>
+        </div>
+
+
+        <div className='card-main'>
+          <FlipCard>
+            <FlipCard.Front className="cardFront card">
+              <h3>FEATURE NAME</h3>
+            </FlipCard.Front>
+            <FlipCard.Back className="cardBack card">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dicta, quasi soluta deleniti, quam sunt dolorum, impedit magnam tempora suscipit quod error fugit temporibus omnis hic ullam modi corrupti placeat?</p>
+            </FlipCard.Back>
+          </FlipCard>
+        </div>
+
+
+        <div className='card-main'>
+          <FlipCard>
+            <FlipCard.Front className="cardFront card">
+              <h3>FEATURE NAME</h3>
+            </FlipCard.Front>
+            <FlipCard.Back className="cardBack card">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dicta, quasi soluta deleniti, quam sunt dolorum, impedit magnam tempora suscipit quod error fugit temporibus omnis hic ullam modi corrupti placeat?</p>
+            </FlipCard.Back>
+          </FlipCard>
+        </div>
+
+
     </div>
   </div>
         </>
